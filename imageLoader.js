8677811ddfb5526f529ac29d8aa8113ec0d5ca64
Erlendbro <erlendbro17@hotmail.com>
2020-10-23 16:45:06 +0200

    UI update
    Just for fun

--- imageLoader.js
+++ imageLoader.js
@@ -1,12 +1,10 @@
-
 // Runs when submit button is pressed
-function submitButton () {
+function submitButton() {
   category = document.getElementById("user_input").value;
 
   if (category == "") alert("Enter an emotion (sad/happy");
-  else
-    spawnImage(category, getRandomInt(0,3),"imgPos");
-  }
+  else spawnImage(category, getRandomInt(0, 3), "imgPos");
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -15,20 +13,22 @@ function getRandomInt(min, max) {
 }
 
 // Spawn image from path and add to page
-function spawnImage(category, number,id) {
-
+function spawnImage(category, number, id) {
   // Delete old image
   if (document.getElementById("imgPos").childElementCount == 0) {
   } else clearImages();
 
   var x = document.createElement("IMG");
-  x.setAttribute("src", ("img/" + category + "/" + number + ".jpg"));
+  x.setAttribute("src", "img/" + category + "/" + number + ".jpg");
   x.setAttribute("alt", category);
+  x.setAttribute("class", "meme");
   document.getElementById(id).appendChild(x);
 }
 
 function clearImages() {
-  var list = document.getElementById("imgPos"); 
-  list.removeChild(list.childNodes[0]); 
+  var list = document.getElementById("imgPos");
+  list.removeChild(list.childNodes[0]);
 
+  var list = document.getElementById("output");
+  list.removeChild(list.childNodes[0]);
 }
