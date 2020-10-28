ff4e5255f33f7ab83cbaed1c87dd15ffee0d608d
Erlendbro <erlendbro17@hotmail.com>
2020-10-28 14:36:56 +0100

    Image loader

--- imageLoader.js
+++ imageLoader.js
@@ -2,8 +2,10 @@
 function submitButton() {
   category = document.getElementById("user_input").value;
 
-  if (category == "") alert("Enter an emotion (sad/happy");
-  else spawnImage(category, getRandomInt(0, 3), "imgPos");
+  // if (category == "") alert("Enter an emotion (sad/happy");
+  // Spawn Image ("emote", 0-max, "position")
+  // else spawnImage(category, getRandomInt(0, 3), "imgPos");
+  userAction(category);
 }
 
 function getRandomInt(min, max) {
@@ -29,6 +31,36 @@ function clearImages() {
   var list = document.getElementById("imgPos");
   list.removeChild(list.childNodes[0]);
 
-  var list = document.getElementById("output");
-  list.removeChild(list.childNodes[0]);
+  var list2 = document.getElementById("output");
+  list.removeChild(list2.childNodes[0]);
+}
+
+const userAction = async (searchTag) => {
+  let tag = searchTag;
+  let apiUrl =
+    "//api.giphy.com/v1/gifs/search?api_key=AlTeXUdZH7iTL5hiHLBUHXeTdBXSL0m6&q=" +
+    tag +
+    "&limit=10&offset=0&rating=g&lang=en";
+  const response = await fetch(apiUrl);
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+
+  // get a random url relevant to category
+  const gifUrl = myJson.data[getRandomInt(0, 10)].images.original.url;
+
+  spawnGif(gifUrl);
+};
+
+// Spawn image from api
+function spawnGif(url) {
+  var x = document.createElement("IMG");
+  x.setAttribute("src", url);
+  x.setAttribute("alt", category);
+  x.setAttribute("class", "meme");
+  document.getElementById("imgPos").appendChild(x);
+
+  if (document.getElementById("imgPos").childElementCount > 1) {
+    var list = document.getElementById("imgPos");
+    list.removeChild(list.childNodes[0]);
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,61 +1,50 @@
-function setup() 
-{
+function setup() {
   noCanvas;
   let speech = new p5.Speech(); //object for synthesis
 
-  let speechRec = new p5.SpeechRec('en-US', gotSpeech); //speech recon object
+  let speechRec = new p5.SpeechRec("en-US", gotSpeech); //speech recon object
   let continuous = true;
   let interim = false;
   speechRec.start(continuous, interim);
 
-
-
-
   let bot = new RiveScript();
   bot.loadFile("brain.rive", brainReady, brainError);
 
-  function brainReady()
-  {
+  function brainReady() {
     console.log("Chatbot Ready");
     bot.sortReplies();
-
   }
 
-  function brainError()
-  {
+  function brainError() {
     console.log("Chatbot error");
   }
 
-let button = select('#submit');
+  let button = select("#submit");
 
-let user_input = select('#user_input');
+  let user_input = select("#user_input");
 
-let output = select ('#output');
+  let output = select("#output");
 
-button.mousePressed(chat);
+  button.mousePressed(chat);
 
-  function chat()
-  {
+  function chat() {
     let input = user_input.value();
-    let username = 'local-user';
-    
-    }
+    let username = "local-user";
+  }
 
-    
-    function gotSpeech()
-    {
-      if(speechRec.resultValue)
-      {
-        let input = speechRec.resultString;
-     
-        console.log(input);
-        user_input.value(input);
-        bot.reply('local-user', input).then(function(reply) {
-          output.html(reply);
-          speech.speak(reply);
-        });
+  function gotSpeech() {
+    if (speechRec.resultValue) {
+      let input = speechRec.resultString;
 
-      }
+      console.log(input);
+      user_input.value(input);
+      bot.reply("local-user", input).then(function (reply) {
+        category = input;
+        userAction(input);
+
+        output.html(reply);
+        speech.speak(reply);
+      });
     }
+  }
 }
-
